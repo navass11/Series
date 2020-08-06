a6a1af57eb3dbeb354d377f3f5c9bd7d6e050e43
navass11 <navass@unican.es>
2020-08-06 13:47:11 +0200

    Update

---
+++ notebooks/Cambio-Climatico/ESFG/ESFG_Download.py
@@ -0,0 +1,91 @@
+# _Autor:_    __Salavador Navas__
+# _Revisión:_ __05/08/2020__
+
+
+import os
+import xarray as xr
+from pyesgf.logon import LogonManager
+from pyesgf.search import SearchConnection
+import tqdm
+import numpy as np
+from netCDF4 import Dataset
+from math import *
+
+def download_ESGF_data(Open_ID, password, server, project, experiment,time_frequency, variable, domain, path_output):
+    """Esta función nos permite descargar masivamente mediante WGET los diferentes ficheros netcdf que contienen los servidrores de ESGF sobre cambio climático.
+    
+    El servidor por defecto que se va a utilizar es: https://esgf-data.dkrz.de/projects/esgf-dkrz/.
+    
+    Parámetros:
+    ---------------------
+    Open_ID         : string. ID de tu usuario para acceder a la base de datos correspondiente del servidor
+    password        : string. Contraseña correspondiente a la ID
+    server          : string. Servidor del que se desea descargar la información. Ejemplo: https://esgf-data.dkrz.de/esg-search
+    project         : string. Proyecto dentro del servidor del que se quiere descargar los datos. Ejemplo: CORDEX, CMIP5, CMIP6
+    experiment      : string. Escenarios de cambio climático. Ejemplo: historical, rcp26, rcp45, rcp85
+    time_frequency  : string. Frecuencia de la base de datos que se quiere. Ejemplo: 1hr, 6hr, day, mon
+    variable        : string. Variable que se desea descargar: tasmax, tasmin, pr 
+    domain          : string. En el caso de que se desee descargar CORDEX, se debe de incluir el nombre de la malla. Ejemplo: EUR-11
+    path_output     : string. Directorio donde se desean guardar los ficheros
+    
+    Salidas:
+    ----------------------
+    Ficheros netcdf para cada uno de los escenarios y modelos solicitados
+    
+    """
+    conn = SearchConnection('https://esgf-data.dkrz.de/esg-search', distrib=True)
+    lm = LogonManager()
+    lm.logoff()
+    lm.is_logged_on()
+    lm.logon_with_openid(Open_ID, password)
+    lm.is_logged_on()
+    if project=='CORDEX':
+        ctx = conn.new_context(
+        project=project,
+        experiment=experiment,
+        time_frequency=time_frequency,
+        variable=variable,
+        domain=domain,)
+    else:
+        ctx = conn.new_context(
+        project=project,
+        experiment=experiment,
+        time_frequency=time_frequency,
+        variable=variable,)
+        
+    with open('wget-plantilla-ESGF.sh', "r+") as out_file:
+        lines = out_file.readlines()
+        
+    lines_first=lines[:27]
+    lines_end=lines[28:]
+    
+    for ct in tqdm.tqdm(range(ctx.hit_count)):
+        files_list=list()
+        result = ctx.search()[ct]
+        lines[23]="earch_url=https://esgf-data.dkrz.de/esg-search/wget/?distrib=false&dataset_id="+result.dataset_id+'\n'
+
+        files = result.file_context().search()
+        ntcf_ds=list()
+        ntcf_name=list()
+        for file in files:
+            if variable in file.opendap_url:
+                files_list.append("'"+file.filename+"'"+' '+"'"+file.download_url+"'"+' '+"'"+file.checksum_type+"'"+' '+"'"+file.checksum+"'"+'\n')
+        with open(path_output+"Download.sh", "w") as fh:
+            for line in (lines_first+files_list+lines_end):
+                fh.write(line)
+                
+        conn = SearchConnection('https://esgf-data.dkrz.de/esg-search', distrib=True)
+        lm = LogonManager()
+        lm.logoff()
+        lm.is_logged_on()
+
+        lm.logon_with_openid(Open_ID, password)
+        lm.is_logged_on()
+        os.chdir(path_output)
+        os.system('bash '+'Download.sh'+' H '+Open_ID+' '+ password)   
+        
+        
+
+
+
+
